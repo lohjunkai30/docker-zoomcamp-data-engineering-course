e45f3b7f77d378276ca27187bb10d32e7e0f0d11
Loh Jun Kai <45227774+lohjunkai30@users.noreply.github.com>
2026-02-18 13:46:17 +0000

    Updated ingestion script to read parquet

--- pipeline/ingest_py2.py
+++ pipeline/ingest_py2.py
@@ -3,7 +3,9 @@
 
 # In[1]:
 
-
+import pyarrow.parquet as pq
+import fsspec
+import os
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -23,23 +25,38 @@ def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, chunksize, url)
 
     engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
-    df_iter = pd.read_csv(
-        url,
-        iterator=True,
-        chunksize=chunksize
-    )
+    if url.endswith('.parquet') or url.endswith('.pq'):
+        with fsspec.open(url) as f:
+            parquet_file = pq.ParquetFile(f)
+            first = True
+            with tqdm(desc=f"Ingesting {target_table}", unit="batch") as pbar:
+                for batch in parquet_file.iter_batches(batch_size = chunksize):
+                    df = batch.to_pandas()
+                    if first:
+                        df.to_sql(name=target_table, con=engine, if_exists = 'replace')
+                        first = False
+                    else:
+                        df.to_sql(name=target_table, con=engine, if_exists = 'append')
+                    pbar.update(1)
 
-    first = True
-    for df_chunk in tqdm(df_iter):
-        if first:
-            df_chunk.head(0).to_sql(target_table, 
-            con=engine, 
-            if_exists='replace')
-            first = False
-            print('Table created')
+    elif url.endswith('.csv'):
+        df_iter = pd.read_csv(
+            url,
+            iterator=True,
+            chunksize=chunksize
+        )
 
-        df_chunk.to_sql(target_table, con=engine, if_exists='append')
-        print("Inserted", len(df_chunk))
+        first = True
+        for df_chunk in tqdm(df_iter):
+            if first:
+                df_chunk.head(0).to_sql(target_table, 
+                con=engine, 
+                if_exists='replace')
+                first = False
+                print('Table created')
+
+            df_chunk.to_sql(target_table, con=engine, if_exists='append')
+            print("Inserted", len(df_chunk))
 
 
 if __name__ == '__main__':
